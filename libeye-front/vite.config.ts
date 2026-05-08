f415f8c150c036eece12e7a1631d73b8dbecdc9b
junghwan020914-maker <junghwan020914@gmail.com>
2026-05-08 19:01:02 +0900

    test

--- libeye-front/vite.config.ts
+++ libeye-front/vite.config.ts
@@ -6,11 +6,17 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    // 💡 ngrok 호스트 차단 해제
+    allowedHosts: ['pyramidlike-distendedly-debroah.ngrok-free.dev'],
     proxy: {
       '/api': {
-        // 백엔드 컨테이너가 열어둔 우분투 IP와 포트로 토스!
+        // 백엔드 컨테이너가 열어둔 우분투 IP와 포트
         target: 'http://210.94.222.176:8000', 
         changeOrigin: true,
       }
+    },
+    watch: {
+      usePolling: true,
     }
   }
+})
