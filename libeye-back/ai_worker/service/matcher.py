ee353576429ab74cbb1d2d3541ed5220cb795b99
dongdong-guk <https://github.com>
2026-06-04 15:03:02 +0900

    test 7 3

--- libeye-back/ai_worker/service/matcher.py
+++ libeye-back/ai_worker/service/matcher.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import Session
 
 from models import BookMaster
 
-WEIGHT_CALL_NUM = 0.8
-WEIGHT_TITLE = 0.2
+WEIGHT_CALL_NUM = 0.7
+WEIGHT_TITLE = 0.3
 MATCH_THRESHOLD = 80.0
 
 def get_top_candidates(db: Session, ocr_call_number: str, limit: int = 5) -> List[BookMaster]:
