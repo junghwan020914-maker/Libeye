17465cdd0ce1b06630995816dc56172160b79445
dongdong-guk <fq6fhg7zjs@privaterelay.appleid.com>
2026-06-14 23:12:28 +0900

    업로드 파일 용량 압축 수정 - 비율 0.99로 시도

--- libeye-front/src/views/CameraView.vue
+++ libeye-front/src/views/CameraView.vue
@@ -245,7 +245,7 @@ const applyCropAndUpload = async () => {
     isCropping.value = false;
     previewUrl.value = null;
     isConfirming.value = true;
-  }, 'image/jpeg', 1.0);
+  }, 'image/jpeg', 0.99);
 };
 
 // 크롭 취소 (기존 인라인 핸들러를 메서드로 분리)
